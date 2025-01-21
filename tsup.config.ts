5ec2fd2659ce5d1f4a1781fa4732f18a6044d247
kabeep <zzx2067@outlook.com>
2025-01-21 18:34:23 +0800

    build(tsup): rm duplicate type declaration file to reduce package size

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,10 @@
+import { accessSync, constants as fsMode, unlinkSync as remove } from 'node:fs';
+import { resolve as pathResolve } from 'node:path';
 import { defineConfig } from 'tsup';
+import { magenta } from './src';
+
+const resolve = (...args: string[]) => pathResolve(__dirname, ...args);
+const print = (label: string) => (text: string) => console.log(label, text);
 
 export default defineConfig((opts) => ({
     entry: ['src/index.ts'],
@@ -15,4 +21,49 @@ export default defineConfig((opts) => ({
     legacyOutput: false,
     replaceNodeEnv: true,
     dts: true,
+    async onSuccess() {
+        if (opts.watch) return;
+
+        const label = magenta('HOOK');
+        const log = print(label);
+
+        // The `tsup` will emit separate d.ts and d.mts declarations starting from version 8.0.1,
+        // but this package does not need it currently. It will only increase the package size.
+        // See https://github.com/egoist/tsup/pull/934
+        const srcMtsFile = 'dist/index.d.mts';
+        const srcMtsFilePath = resolve(srcMtsFile);
+        const isExists = await wait(srcMtsFilePath);
+        if (!isExists) return;
+        remove(srcMtsFilePath);
+        log(`rm ${magenta(srcMtsFile)}`);
+    },
 }));
+
+function sleep(interval: number) {
+    return new Promise<void>((res) => {
+        setTimeout(() => {
+            res();
+        }, interval);
+    });
+}
+
+/** Exists sync for file or directory */
+function access(path: string) {
+    try {
+        accessSync(path, fsMode.F_OK);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+/** Wait for file */
+async function wait(path: string, timeout = 5000, interval = 1000) {
+    let poll = Math.floor(timeout / interval);
+    if (poll <= 0) return false;
+
+    while (poll--) {
+        if (access(path)) return true;
+        await sleep(interval);
+    }
+}
