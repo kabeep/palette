fe28ad9e16dd916adf56cdadd200aaad6681238f
kabeep <zzx2067@outlook.com>
2025-01-21 19:55:44 +0800

    test(CI): debug pipeline p2

--- test/debug.spec.ts
+++ test/debug.spec.ts
@@ -1,6 +1,5 @@
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import { rgb } from '../src';
-import * as constants from '../src/constants';
 
 describe('createAnsi16', () => {
     afterEach(() => {
@@ -8,12 +7,10 @@ describe('createAnsi16', () => {
     });
 
     it('With color supported', () => {
-        vi.spyOn(constants, 'IS_16_COLORS_SUPPORTED', 'get').mockReturnValue(
-            true,
-        );
-        vi.spyOn(constants, 'IS_16M_COLORS_SUPPORTED', 'get').mockReturnValue(
-            true,
-        );
+        vi.spyOn(
+            require('node:tty').WriteStream.prototype,
+            'hasColors',
+        ).mockReturnValue(true);
 
         expect(rgb(255, 255, 255)('debug')).toStrictEqual(
             '\x1b[38;2;255;255;255mdebug\x1b[39m',
@@ -21,12 +18,10 @@ describe('createAnsi16', () => {
     });
 
     it('Without color supported', () => {
-        vi.spyOn(constants, 'IS_16_COLORS_SUPPORTED', 'get').mockReturnValue(
-            false,
-        );
-        vi.spyOn(constants, 'IS_16M_COLORS_SUPPORTED', 'get').mockReturnValue(
-            false,
-        );
+        vi.spyOn(
+            require('node:tty').WriteStream.prototype,
+            'hasColors',
+        ).mockReturnValue(true);
 
         expect(rgb(255, 255, 255)('debug')).toStrictEqual('debug');
     });
